c513b86c251421aa872a4247bb304d6385fdc236
Luis Fernando Salas Gave <lsalagav@emeal.nttdata.com>
2022-12-14 23:39:55 -0500

    Add Search Favorite Book feature

--- BookOutlet/ViewModel/ViewController.swift
+++ BookOutlet/ViewModel/ViewController.swift
@@ -7,25 +7,36 @@
 
 import UIKit
 
-
-
-
 class ViewController: UIViewController {
     //Outlets
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activity: UIActivityIndicatorView!
     
     var data = [List]()
+    var books = [List]()
     
     //instancia de modelo de la vista
     var viewModel = ViewModelList()
     
+    //Search Handler
+    @IBAction func searchHandler(_ sender: UITextField) {
+        if let searchText = sender.text {
+            books = searchText.isEmpty ? data :
+            data.filter{$0.author.lowercased().contains(searchText.lowercased()) ||
+                $0.title.lowercased().contains(searchText.lowercased())}
+            tableView.reloadData()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.books = data
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
         configureView()
+        //self.books = self.data
         activity.isHidden = false
     }
     
@@ -34,6 +45,7 @@ class ViewController: UIViewController {
         viewModel.retrieveDataList(URL: "https://fernando-bookstore.netlify.app/.netlify/functions/api/books") { result in
             self.viewModel.dataArray = result
             self.data = result
+            self.books = self.data
             DispatchQueue.main.async {
                 self.tableView.reloadData()
                 self.activity.startAnimating()
@@ -48,12 +60,14 @@ class ViewController: UIViewController {
 
 extension ViewController:  UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.dataArray.count
+        //return viewModel.dataArray.count
+        return books.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
-        let info = viewModel.dataArray[indexPath.row]
+        //let info = viewModel.dataArray[indexPath.row]
+        let info = books[indexPath.row]
         cell.titleLabel.text = info.title
         cell.authorLabel.text = info.author
         let string = info.image
@@ -67,24 +81,3 @@ extension ViewController:  UITableViewDelegate, UITableViewDataSource {
 
 
 
-//Download Image
-extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
-    }
-}
